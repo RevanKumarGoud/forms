fc359e85074dbc73243a74cc86ce77c237708694
[RevankumarGoud] <[revankumargoudkurmindla@gmail.com]>
2022-07-27 23:55:41 +0530

    some changing forms anding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,28 @@ import { User } from './user';
 })
 export class AppComponent {
   topics = ['Angular', 'vue', 'react'];
+  topicHasError =true;
+  submitted = false;
+errorMsg = '';
+
   userModel = new User('', '', '', 199286, 9948109968, '');
+
+ constructor(private _enrollmentService : EnrollmentService){}
+
+  VaildationTopic(value:any){
+     if(value === 'default') {
+      this.topicHasError =true;
+     }else {
+      this.topicHasError = false
+     }
+  }
+
+  onsubmit(){
+    this.submitted = false;
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+           data => console.log('success!',data),
+           error => this.errorMsg = error.statusText
+    )
+  }
 }
